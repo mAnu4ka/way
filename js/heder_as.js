37b360cb4228ebff27aec76c829241905b30294e
mAnu4ka <kompukterrrr@gmail.com>
2022-05-26 17:47:22 +0500

    всё есть всё наместе

---
+++ js/heder_as.js
@@ -0,0 +1,26 @@
+let header = `<header class="max-width">
+<div class="_icon-logo logo__main"></div>
+<div class="raight">
+    <div class="_icon-search"></div>
+    <input type="text" placeholder="Найти..."></input>
+    <div class="_icon-like"></div>
+    <div class="_icon-cart_head">
+        <div class="boot">
+            <div class="_icon-cart_wheel"></div>
+            <div class="_icon-cart_wheel"></div>
+        </div>
+    </div>
+    <div class="_icon-user"></div>
+</div>
+</header>
+<aside class="max-width pc-element__890">
+<ul>
+    <li class="active">Домой</li>
+    <li>Магазин</li>
+    <li>Коллекции</li>
+    <li>Коллаборации</li>
+    <li>о нас</li>
+</ul>
+</aside>`
+let all__prodaction = document.querySelector('.all__prodaction')
+all__prodaction.innerHTML = all__prodaction
